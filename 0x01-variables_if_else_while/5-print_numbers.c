578fa16303314ed0da3315e9c07b24064c502b99
SiboneloShezi <wizardshezi@gmail.com>
2023-02-17 15:44:10 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n < 10 ; n++)
+printf("%d\n", n);
+return (0);
+}
