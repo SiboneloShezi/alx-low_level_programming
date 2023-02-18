635e2fce92e5f07ed58020b9f6b9698a84648bc8
SiboneloShezi <wizardshezi@gmail.com>
2023-02-17 17:30:58 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+int b;
+
+for (b = 0 ; b < 10; b++)
+putchar(b + '0');
+if (b < 9)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
