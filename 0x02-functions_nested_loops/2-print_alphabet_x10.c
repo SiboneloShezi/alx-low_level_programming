313031df502a2159b3506f2c7103b9fa57f4f23c
SiboneloShezi <wizardshezi@gmail.com>
2023-02-21 11:41:38 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@
 void print_alphabet_x10(void)
 {
 char i;
-int n;
 
-for (n = 0; n < 10; n++)
+int n = 0;
+while (n < 10)
 {
 for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 }
 _putchar('\n');
+n++;
 }
