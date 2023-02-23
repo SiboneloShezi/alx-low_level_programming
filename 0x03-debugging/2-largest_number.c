4c104a039db3d6a405947f85c9f3d7fb1816bd0e
SiboneloShezi <wizardshezi@gmail.com>
2023-02-23 06:24:58 +0200

    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

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,25 +8,24 @@
 * Return: largest number
 */
 
-int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (c > a && a > b)
+else if (a > b && c > a)
 {
 largest = c;
 }
-else if (b > a && a > c)
+else if (b > c)
 {
 largest = b;
 }
 else
 {
-largest = b;
+largest = c;
 }
 
 return (largest);
