b0c503612f7b71136b9e1a34ff3bea42858b3113
SiboneloShezi <wizardshezi@gmail.com>
2023-02-23 17:23:11 +0200

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+* check for uppercase alphabets 
+* @c: Variable text
+* return 1 if c is uppercase 
+*/
+
+void _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'z')
+{
+return (1);
+}
+else 
+{
+return (0); 
+}
