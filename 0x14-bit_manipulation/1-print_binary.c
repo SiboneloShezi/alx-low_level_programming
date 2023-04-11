b8e301252f83c7bce851220ef532056905611ea9
SiboneloShezi <wizardshezi@gmail.com>
2023-04-12 01:00:27 +0200

    1-print_binary.c task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+1#include "main.h"
+
+/**
+* print_binary - prints the binary equivalent of a decimal number
+* @n: number to print in binary
+*/
+void print_binary(unsigned long int n)
+{
+int z, tally = 0;
+unsigned long int new;
+
+for (z = 63; z >= 0; z--)
+{
+new = n >> z;
+
+if (new & 1)
+{
+_putchar('1');
+tally++;
+}
+else if (tally)
+_putchar('0');
+}
+if (!tally)
+_putchar('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
-MAIN.H
+main.h
 
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
